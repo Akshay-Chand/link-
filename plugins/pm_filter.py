64932d3cdd2b0f5aadbccc736e57c84154f7db52
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2022-12-05 15:20:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -4,7 +4,7 @@ import re
 import ast
 import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from Script import script M_NT_FND
+from Script import script, M_NT_FND
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
@@ -149,7 +149,7 @@ async def advantage_spoll_choker(bot, query):
                parse_mode='html'
 )
 
-           await asyncio.sleep(15)
+           await asyncio.sleep(10)
            await k.delete()
 
 
